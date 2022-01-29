dfa5a6da8ffd3cdd0fb6982f73c79e004f684acd
Juliano Gomes <julianocg2013@gmail.com>
2022-01-28 23:53:14 -0300

    Projeto criado.

---
+++ src/main/java/com/projectW/ProjectWApplication.java
@@ -0,0 +1,13 @@
+package com.projectW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectW/ProjectWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
