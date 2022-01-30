a7d0ad9f24ae791d40385d3c579251df23354cda
Juliano Gomes <julianocg2013@gmail.com>
2022-01-30 20:25:21 -0300

    Criado operacao de instaciacao - Categoria

--- src/main/java/com/projectW/ProjectWApplication.java
+++ src/main/java/com/projectW/ProjectWApplication.java
@@ -1,13 +1,33 @@
 package com.projectW;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjectWApplication {
+import com.projectW.domain.Categoria;
+import com.projectW.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjectWApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository catRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "TI");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
