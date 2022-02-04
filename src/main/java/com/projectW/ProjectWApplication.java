30f4f8df55514708af3d93ab385da56a4b34d943
Juliano Gomes <julianocg2013@gmail.com>
2022-02-03 21:50:11 -0300

    Cliente, Tipo Cliente, Endereco e Telefone

--- src/main/java/com/projectW/ProjectWApplication.java
+++ src/main/java/com/projectW/ProjectWApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.projectW.domain.Categoria;
 import com.projectW.domain.Cidade;
+import com.projectW.domain.Cliente;
+import com.projectW.domain.Endereco;
 import com.projectW.domain.Estado;
 import com.projectW.domain.Produto;
+import com.projectW.domain.enums.TipoCliente;
 import com.projectW.repositories.CategoriaRepository;
 import com.projectW.repositories.CidadeRepository;
+import com.projectW.repositories.ClienteRepository;
+import com.projectW.repositories.EnderecoRepository;
 import com.projectW.repositories.EstadoRepository;
 import com.projectW.repositories.ProdutoRepository;
 
@@ -31,6 +36,12 @@ public class ProjectWApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EstadoRepository estRepository;
+	
+	@Autowired
+	private ClienteRepository cliRepository;
+	
+	@Autowired
+	private EnderecoRepository endRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWApplication.class, args);
@@ -67,8 +78,19 @@ public class ProjectWApplication implements CommandLineRunner{
 		estRepository.saveAll(Arrays.asList(est1, est2));
 		cidRepository.saveAll(Arrays.asList(cid1, cid2, cid3, cid4)); 
 		
+		Cliente cli1 = new Cliente(null, "Teste", "teste@teste", "243234234234", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("12312312312", "a123123123"));
 		
+		Endereco e1 = new Endereco(null, "Assis Brasil", "300", "Apto 200", "Sarandi", "12312312", cli1, cid1);
+		Endereco e2 = new Endereco(null, "Assis Brasil", "300", "Apto 12", "Centro", "123123123", cli1, cid2);
 		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		//Cliente cli2 = new Cliente(null, "Teste", "teste@teste", "243234234234", TipoCliente.PESSOAFISICA);
+		//Cliente cli3 = new Cliente(null, "Teste", "teste@teste", "243234234234", TipoCliente.PESSOAFISICA);
+		
+		cliRepository.saveAll(Arrays.asList(cli1));
+		endRepository.saveAll(Arrays.asList(e1, e2));
 		
 		
 	}
---
+++ src/main/java/com/projectW/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.projectW.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projectW.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
